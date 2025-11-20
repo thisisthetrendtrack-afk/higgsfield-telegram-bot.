fc907bf4d92d150dc1ce1e641487ea0843244d7e
thisisthetrendtrack-afk <thisisthetrendtrack@gmail.com>
2025-11-20 19:03:37 +0400

    main.py

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 import os
 from telegram.ext import ApplicationBuilder
-from bot import start_handler, button_handler, message_handler
+from bot import register_handlers
 
 
 def main():
-    BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
     HF_KEY = os.getenv("HF_KEY")
     HF_SECRET = os.getenv("HF_SECRET")
 
     if not BOT_TOKEN:
-        raise RuntimeError("TELEGRAM_TOKEN missing in Railway Variables")
+        raise RuntimeError("BOT_TOKEN missing in Railway Variables")
     if not HF_KEY or not HF_SECRET:
         raise RuntimeError("HF_KEY or HF_SECRET missing in Railway Variables")
 
-    print("Starting HiggsMasterBot...")
+    print("Starting Higgsfield Cloud Bot...")
 
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    # add handlers
-    app.add_handler(start_handler)
-    app.add_handler(button_handler)
-    app.add_handler(message_handler)
+    register_handlers(app)
 
-    print("Bot is now running…")
     app.run_polling()
 
 
